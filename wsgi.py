ece18b34a501adba9cd0e53d1a7a48da2b0d02b3
hoanbell <69511412+hoanbell@users.noreply.github.com>
2020-10-06 10:10:38 +0700

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,12 @@
-from app.main import app
+from flask import Flask
 
-if __name__ == "__main__":
-	app.run()
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return "<h1>Welcome to Summoner's Drift</h1>"
+
+
+if __name__ == '__main__':
+    app.run(debug=1, host='0.0.0.0')
